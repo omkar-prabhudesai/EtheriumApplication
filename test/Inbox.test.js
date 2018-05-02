e2dfb40f986285217edd73080612d04240cbd754
Omkar Prabhudesai <omkar.prabhudesai@gmail.com>
2018-05-02 15:37:50 +0530

    Initial commit - basic project structure created

---
+++ test/Inbox.test.js
@@ -0,0 +1,5 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+//by convention if we want to initilize with constructor, capitalize it
+//shown below i.e. W of web3
+const Web3 = require('web3')

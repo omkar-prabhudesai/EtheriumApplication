cfe572de8eaf186b59f1972c45f3edd68a870e46
Omkar Prabhudesai <omkar.prabhudesai@gmail.com>
2018-05-05 16:51:02 +0530

    Completed tests development

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,24 +3,39 @@ const ganache = require('ganache-cli')
 //by convention if we want to initilize with constructor, capitalize it
 //shown below i.e. W of web3
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
+const provider = ganache.provider()
+const web3 = new Web3(provider)
 const{interface,bytecode} = require('../compile');
 
 let accounts;
 let inbox;
+let INITIAL_MESSAGE = 'Hi There !'
 beforeEach(async ()=>{
     //Get a list of all accounts
    accounts = await web3.eth.getAccounts()
     //Use one of that account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data:bytecode,arguments:['Hi There!']})
+        .deploy({data:bytecode,arguments:[INITIAL_MESSAGE]})
         .send({from:accounts[0],gas:1000000});
+    
+    inbox.setProvider(provider);
 });
 
 describe('Inbox',()=>{
 
-    it('Deploys contract',()=>{
-        console.log(inbox)
+    it ('Deploys contract',()=>{
+        assert.ok(inbox.options.address);
+    })
+
+    it ('Has a default message',async()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message,INITIAL_MESSAGE)
+    })
+
+    it ('can change message',async()=>{
+        await inbox.methods.setMessage('bye').send({from:accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'bye')
     })
 });
 
